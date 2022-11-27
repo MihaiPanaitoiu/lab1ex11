5052abd7da648ed02ffc52fd282b8f08cb215b4d
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 20:46:20 +0200

    Add project files.

---
+++ lab1ex11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab1ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Num = ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            /*
+  switch (num)
+{
+    case 1:
+        str = "unu";
+        break;
+    case 2:
+        str = "doi";
+        break;
+    case 3:
+        str = "trei";
+        break;
+    case 5:
+        str = "cinci";
+        break;
+    case 8:
+        str = "opt";
+        break;
+    default:
+        str = "neidentificat";
+        break;
+}
+ */
+
+            string str = num switch
+            {
+                1 => "unu",
+                2 => "doi",
+                3 => "trei",
+                5 => "cinci",
+                8 => "opt",
+                _ => "neidentificat",
+            };
+
+            Console.WriteLine(str);
+        }
+    }
+}
